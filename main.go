e8236b861bb0ecd99c711375dbb0a5537641c869
Part001-R <sec001.mail002@gmail.com>
2025-06-04 13:10:14 +0700

    Вывод данных пользователя

--- main.go
+++ main.go
@@ -9,14 +9,13 @@ import (
 )
 
 type User struct {
-	Name       string
-	Age        uint16
-	Money      int16
-	Avg_grades float64
-	Happiness  float64
-	Hobbies    []string
+	Name       string   `json:"Name"`
+	Age        uint16   `json:"Age"`
+	Money      int16    `json:"Money"`
+	Avg_grades float64  `json:"Avg_grades"`
+	Happiness  float64  `json:"Happiness"`
+	Hobbies    []string `json:"Hobbies"`
 }
-
 type Message struct {
 	Message string `json:"message"`
 }
@@ -24,7 +23,8 @@ type Message struct {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home_page)
-	mux.HandleFunc("/button_click", buttonClickHandler)
+	mux.HandleFunc("/button_click", hndlRxData)
+	mux.HandleFunc("/user_info", hndlTxUserInfo)
 
 	fs := http.FileServer(http.Dir("templates/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
@@ -33,42 +33,56 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
+// Вывод домашней страницы
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{
-		Name:       "Bob",
-		Age:        25,
-		Money:      -50,
-		Avg_grades: 4.2,
-		Happiness:  0.8,
-		Hobbies:    []string{"Football", "Skate", "Dance"},
-	}
 
 	tmpl, err := template.ParseFiles("templates/home_page.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	err = tmpl.Execute(w, bob)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
-func buttonClickHandler(w http.ResponseWriter, r *http.Request) {
+// Приём данных
+func hndlRxData(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-
 		var msg Message
 		err := json.NewDecoder(r.Body).Decode(&msg)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 			return
 		}
-
 		fmt.Println("Получено сообщение:", msg.Message)
 		w.WriteHeader(http.StatusOK)
 	} else {
-		http.Error(w, "Метод не разрешен", http.StatusMethodNotAllowed)
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+// Передача информации о пользователе
+func hndlTxUserInfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		bob := User{
+			Name:       "Пётр",
+			Age:        25,
+			Money:      -50,
+			Avg_grades: 4.2,
+			Happiness:  0.8,
+			Hobbies:    []string{"Go", "Кино", "Футбол"},
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(bob)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
 }
